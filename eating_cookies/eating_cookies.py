96ac56d9042575e889c918b4cd1b83e30d602542
David Tauraso <david318@me.com>
2020-04-15 10:37:26 -0700

    Eating cookies is done

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -30,14 +30,25 @@ def eating_cookies(n, cache=None):
   4:
     f(4-3) + f(4-2) + f(4-1)
 
-  '''
 
-  cache = {0: 0, 1: 1, 2: 2, 3: 4}
-  for i in range(1, n):
+  1 cookie 2 times
+  2 cookies 1 time
+  '''
+  # 1 way to eat 0 cookies?
+  cache = {0: 1, 1: 1, 2: 2, 3: 4}
+  # print("n", n)
+  if n < 3:
+    return cache[n]
+  for i in range(1, n+1):
+    
     if(i > 3):
       cache[i] = cache[i - 1] + cache[i - 2] + cache[i - 3]
+  # print(cache, n)
+  # 10562230626642
+  # 10562230626642
+  # 5742568741225
   return cache[n]
-  pass
+  # pass
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
