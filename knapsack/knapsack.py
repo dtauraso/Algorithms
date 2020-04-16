c2e0eb373ab386a90bc738f5401859363b44e7ee
David Tauraso <david318@me.com>
2020-04-16 12:05:28 -0700

    Not done yet

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -10,7 +10,7 @@ def knapsack_solver(items, capacity):
   # print('solver', len(items), capacity)
   items = [Item(0, 0, 0), *items]
 
-  [print(i) for i in items]
+  # [print(i) for i in items]
   capacity_list = [n for n in range(capacity + 1)]
 
   table = []#[[0 for x in range(capacity)] for y in range(len(items))]
@@ -19,20 +19,16 @@ def knapsack_solver(items, capacity):
     for x in range(capacity + 1):
       row.append(0)
     table.append(row)
-    
-  # I think the table is set up slightly wrong
-  # it may also be inittialized incorrectly
-
-  [print(i) for i in table]
-  print(capacity_list)
+  # [print(i) for i in table]
+  # print(capacity_list)
   
-  # exit()
-  print()
+  # # exit()
+  # print()
   # make a fake item (0, 0, 0)
   for i, item in enumerate(items):
     # we make all the possible knapsacks made from items[0,ith item]
     for c, current_capacity in enumerate(capacity_list):
-      print(i, c)
+      # print(i, c)
       # print(item, current_capacity)
       # print(5)
       # print(current_capacity)
@@ -40,23 +36,23 @@ def knapsack_solver(items, capacity):
       item_index = item.index
       item_size = item.size
       item_value = item.value
-      print("capacity", current_capacity, "weight", item_size, 'payoff', item_value)
-      print('difference', current_capacity - item_size)
+      # print("capacity", current_capacity, "weight", item_size, 'payoff', item_value)
+      # print('difference', current_capacity - item_size)
       # exactly right amount(just add the one thing)
       if current_capacity - item_size == 0:
         table[i][c] = item_value
 
-      # no room(discard it and use previous value as it's the best we can do)
+      # no room(discard it and use previous row's value as it's the best we can do)
       elif current_capacity - item_size < 0:
-        table[i][c] = table[i][c - 1]
+        table[i][c] = table[i - 1][c]
 
       # we have room for adding an additional item
       elif current_capacity - item_size > 0:
-        # thought we always had to include the value once we see there is a fit
+        # thought we always had to include the value once we see there is a fit(item_value + table[i - 1][c - item_size])
         # It's wrong because it may have a lower value than the
         # previous capacity for the same item so we must consider 2 options? still confusing
         # maybe I'm just confused at how a lower value would ever show up
-        print(item_value + table[i - 1][c - item_size], table[i][c - 1])
+        # print(item_value + table[i - 1][c - item_size], table[i][c - 1])
         # we want the max value of these 2 possibilities
         table[i][c] = max(
                           # include the value to our last best value(t[i - 1][c - item_size])
@@ -66,11 +62,48 @@ def knapsack_solver(items, capacity):
                           table[i - 1][c])
       # [print(i) for i in items]
       # [print(items[i], row) for i, row in enumerate(table)]
-    print()
+    # print()
     # [print((items[i].index, items[i].size, items[i].value), row) for i, row in enumerate(table)]
-  [print((items[i].index, items[i].size, items[i].value), row) for i, row in enumerate(table)]
+  # print(('i', 'w', 'v'), [i for i in range(capacity + 1)])
+  # [print((f'  {items[i].index} |  {items[i].size} |  {items[i].value}'), " ", row) for i, row in enumerate(table)]
+  '''
+  go back through the table
+  identify all the indexes that point to an item in the solution
 
-  print(capacity)
+  start at table[last_item][c]
+  if table[last_item][c - 1] == table[last_item][c]
+    jumpt up 1 row 
+  '''
+
+  print('retreiving indicies')
+  print('final value', table[len(items)-1][capacity])
+  temp_capacity = capacity
+  ith_item = len(items) - 1
+  indicies = {}
+
+  while(temp_capacity > 0):
+    # print(ith_item, temp_capacity)
+    if(table[ith_item][temp_capacity] == table[ith_item - 1][temp_capacity]):
+      ith_item = ith_item - 1
+      # if(ith_item < 0):
+      #   break
+    # ith_item should point to an item included in the solution
+    # print(ith_item, items[ith_item])
+    # if ith_item < 0:
+    #   break
+    indicies[ith_item] = 1
+    temp_capacity = temp_capacity - items[ith_item].size
+  # print('done')
+  # print(indicies)
+
+  # items_in_bag = [items[index] for i, index in enumerate(indicies)]
+
+  print(indicies)
+  # [print(i) for i in items_in_bag]
+  answer = {'Value': table[len(items) - 1][capacity], 'Chosen': indicies}
+  return answer
+  # return items_in_bag
+  # print(capacity)
   # [print(i) for i in table]
   # pass
 def factorial(value):
@@ -94,6 +127,7 @@ knapsack_solver(items, 7)
 
 
 if __name__ == '__main__':
+  print("testing", sys.argv)
   if len(sys.argv) > 1:
     capacity = int(sys.argv[2])
     file_location = sys.argv[1].strip()
